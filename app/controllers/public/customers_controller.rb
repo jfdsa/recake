f3b97ae3b8fc24e8a5918ec522fecafe37dd0fcc
jfdsa <aieskjakk@gmail.com>
2023-12-08 15:05:34 +0000

    admin out customer?

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,10 +1,38 @@
 class Public::CustomersController < ApplicationController
+  before_action :authenticate_customer!
+  before_action :set_current_customer
+  
   def show
   end
 
   def edit
   end
-
-  def unsubscribe
+  
+  def update
+    if @customer.update(customer_params)
+      redirect_to mypage_path, notice: '会員情報の更新が完了しました。'
+    else
+      render :edit
+    end
   end
+
+  def unsubscribe #退会確認画面
+  end
+  
+  def withdraw
+    @customer.update(is_active: false)
+    reset_session
+    redirect_to root_path
+  end
+  
+  private
+  
+  def set_current_customer
+    @customer=current_customer
+  end
+  
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :first_name_kana, :last_name_kana, :email, :postal_code, :address, :telephone_number)
+  end
+  
 end
